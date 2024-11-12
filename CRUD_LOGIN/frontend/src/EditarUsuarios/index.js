131858cd56134679a2552b3a5995f99fddbd9b13
gabizinhagarcia <gabizinhaleitegarcia@gmail.com>
2024-11-12 16:37:26 -0300

    frontend

--- CRUD_LOGIN/frontend/src/EditarUsuarios/index.js
+++ CRUD_LOGIN/frontend/src/EditarUsuarios/index.js
@@ -1,57 +1,74 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import apilocal from '../Api/apiLocal'
+import { useParams, useNavigate } from 'react-router-dom'
+import apiLocal from './../Api/apiLocal'
 import './estilo.EditarUsuarios.scss'
+import { toast } from 'react-toastify'
 
 export default function EditarUsuarios() {
+    const mudarTela = useNavigate()
     const { id } = useParams()
     const [nome, setNome] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     useEffect(() => {
-        async function consultarDados() {
-            const resposta = await apilocal.post('/ConsultarUsuariosUnico', {
-                id
+        try {
+            async function consultarDados() {
+                const resposta = await apiLocal.post('/ConsultarUsuariosUnico', {
+                    id
+                })
+                setNome(resposta.data.nome)
+                setEmail(resposta.data.email)
+                setPassword(resposta.data.senha)
+            }
+            consultarDados()
+        } catch (err) {
+            toast.error('Erro ao Comunicar com o Servidor', {
+                toastId: 'ToastId'
             })
-            setNome(resposta.data.nome)
-            setEmail(resposta.data.email)
-            setPassword(resposta.data.senha)
         }
-        consultarDados()
     }, [])
 
     async function enviarAlteracao(e) {
-        e.preventDefault()
-        alert(nome + email + password)
+        try {
+            e.preventDefault()
+            const resposta = await apiLocal.put('/AlterarDadosUsuarios', {
+                id,
+                nome,
+                email
+            })
+            toast.success('Cadastro Alterado com Sucesso', {
+                toastId: 'ToastId'
+            })
+            mudarTela('/')                        
+        } catch (err) {
+            toast.error('Erro ao Comunicar com o Servidor', {
+                toastId: 'ToastId'
+            })
+        }       
     }
 
     return (
-        <div className="containerEditarUsuarios">
-            <h1> Editar Usuarios</h1>
-            {id}
+        <div className='conteinerEditarUsuariosGeral'>
+            <h1>Editar Usuários</h1>
             <form onSubmit={enviarAlteracao}>
                 <input
                     type="text"
                     value={nome}
                     onChange={(e) => setNome(e.target.value)}
                 />
-
                 <input
                     type="text"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
-
                 <input
-                disabled
+                    disabled
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-
-                <button type='submit'> enviar </button>
-
+                <button type='submit'>Enviar</button>
             </form>
         </div>
     )
