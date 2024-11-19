50177cdbfd4ba91099dd41926ac94091616f6a1c
gabizinhagarcia <gabizinhaleitegarcia@gmail.com>
2024-11-19 16:57:20 -0300

    frontend

--- CRUD_LOGIN/frontend/src/Inicio/index.js
+++ CRUD_LOGIN/frontend/src/Inicio/index.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import { AutenticadoContexto } from '../contexts/authContexts'
 import { Link } from "react-router-dom"
-
+import {toast} from 'react-toastify'
 import './estilo.inicio.scss'
 
 export default function Inicio() {
+
+    const { loginEntrada } = useContext(AutenticadoContexto)
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function dadosLogin(e){
+        e.preventDefault()
+        if(!email || !password ){
+            toast.warning('preencha todos os campos')
+            return
+        }
+        try {
+            await loginEntrada(email, password)
+        } catch (err) {
+            
+        }
+    }
+
     return (
         <div className='conteinerInicioGeral'>
-            <h1>Pagina de Inicio</h1>
+            <h1>Pagina de Inicio crud login</h1>
 
-            <form>
+            <form onSubmit={dadosLogin}>
                 <input
                     type="text"
                     placeholder='Digite o E-mail'
+                    value= {email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <input
                     type="password"
                     placeholder='Digite a Senha'
+                    value= {password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <button>Enviar</button>
